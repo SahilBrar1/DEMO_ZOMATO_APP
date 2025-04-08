e2e934678dcc26535408f01c88913710f21e9a24
SahilBrar1 <sbrar7282@gmail.com>
2025-04-08 16:04:47 +0530

    revert some changes

--- components/AppButton.tsx
+++ components/AppButton.tsx
@@ -2,10 +2,6 @@ import React from "react";
 import { StyleSheet, TouchableOpacity, Text, View, Image } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import Ratebutton from "./Ratebutton";
-<<<<<<< HEAD
-import SearchBar from "./SearchBar";
-=======
->>>>>>> 54b040124280cf6f87b26e0ceb6f09792398a191
 
 interface AppButtonProps {
   onPress?: () => void;
@@ -14,32 +10,9 @@ interface AppButtonProps {
 }
 
 const AppButton: React.FC<AppButtonProps> = ({ name }) => (
-<<<<<<< HEAD
-  <TouchableOpacity
-    activeOpacity={0.1}
-    style={[
-      styles.appButtonContainer,
-    ]}
-  ><View>
-    </View>
-    
-    <View>
-
-    <Image style={styles.img} source={require('../assets/images/pizzaimg.jpg') } resizeMode="cover"/>
-    </View>
-    <View style={styles.time} >
-    <Ionicons name="time"  />
-    <Text>23 min  </Text>
-   
-    <Text>  2.2km</Text>
-    </View>
-    <View style={styles.textflex} >
-    <Text style={styles.appButtonText}>{name}</Text>
-   <Ratebutton/>
-   </View>
-   
-=======
   <TouchableOpacity activeOpacity={0.1} style={[styles.appButtonContainer]}>
+    <View></View>
+
     <View>
       <Image
         style={styles.img}
@@ -49,7 +22,7 @@ const AppButton: React.FC<AppButtonProps> = ({ name }) => (
     </View>
     <View style={styles.time}>
       <Ionicons name="time" />
-      <Text>23 min</Text>
+      <Text>23 min </Text>
 
       <Text> 2.2km</Text>
     </View>
@@ -57,10 +30,9 @@ const AppButton: React.FC<AppButtonProps> = ({ name }) => (
       <Text style={styles.appButtonText}>{name}</Text>
       <Ratebutton />
     </View>
->>>>>>> 54b040124280cf6f87b26e0ceb6f09792398a191
+
     <Text>flat Rs. 150 off above Rs. 299</Text>
   </TouchableOpacity>
-
 );
 
 export default AppButton;
@@ -72,11 +44,7 @@ const styles = StyleSheet.create({
     borderRadius: "7%",
   },
   appButtonText: {
-<<<<<<< HEAD
-  fontSize:20,
-=======
-    fontSize: 18,
->>>>>>> 54b040124280cf6f87b26e0ceb6f09792398a191
+    fontSize: 20,
     color: "black",
     fontWeight: "bold",
   },
